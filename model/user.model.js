c6a380d936cb1d21c4a2872f349bdd1bca10b918
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-02-24 17:47:55 +0500

    add user model

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const JWT=require("jsonwebtoken")
-const {TOKEN_SECRET_KEY,EXPIRE_DATE }=require("../configs/config")
+const JWT = require("jsonwebtoken");
+const { TOKEN_SECRET_KEY, EXPIRE_DATE } = require("../configs/config");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -31,24 +31,27 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  const Salt =  bcrypt.genSaltSync(11);
+  const Salt = bcrypt.genSaltSync(11);
   this.password = await bcrypt.hash(this.password, Salt);
 });
 
-userSchema.methods.isPasswordCorrect= async function (password){
-    return bcrypt.compareSync(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-userSchema.methods.generateToken=function(){
-    return JWT.sign({
-        id:this._id,
-        username:this.username,
-        email:this.email
-    },TOKEN_SECRET_KEY,{
-      expiresIn:EXPIRE_DATE
+userSchema.methods.generateToken = function () {
+  return JWT.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    TOKEN_SECRET_KEY,
+    {
+      expiresIn: EXPIRE_DATE,
     }
-)
-}
+  );
+};
 
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
