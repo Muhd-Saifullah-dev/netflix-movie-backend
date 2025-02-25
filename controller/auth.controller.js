d1209ba44f3fddc6aa170204fd5e5f3fbd68f035
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-02-25 14:11:40 +0500

    add auth controllers

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,18 +1,46 @@
-const { BadRequestError}=require("../customErrors")
+const { BadRequestError}=require("../customErrors");
+const {User}=require("../model/index")
+const { okResponse}=require("../utils/handlerErrors.util")
 
 const SignupUser=async (req,res,next)=>{
-    const {username,email,password}=req.body
-    if([username,email.password].some((field)=>field === '')){
-        throw new BadRequestError("all field are required")
+    try {
+        const {username,email,password}=req.body
+        let existingUser=await User.findOne({
+            $or:[{email},{username}]
+        }) 
+        if(existingUser){
+            throw new BadRequestError("email or username is already")
+        }
+        const PROFILE_PICS=['/avatar1.png','/avatar2.jpg','/avatar3.png'];
+        const image=PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)]
+        const newUser=await User.create({
+            username,
+            email,
+            password,
+            image
+        })
+        const Token=await newUser.generateToken()
+        okResponse(res,201,"User created Successfully",{user:{name:newUser.username,
+            Image:newUser.image
+        }},{token:Token})
+    } catch (error) {
+        console.error(`error in signup user :: ${error}`)
+        next(error)
     }
-    const EmailRegex= /^[^@]+@[^@]+\.[^@]+$/;
 
-    if(!EmailRegex.test(email)){
-        throw new BadRequestError("Invalid Email")
-    }
-    if(password.length < 6){
-        throw new BadRequestError("password length must be ")
-    }
 }
-const LoginUser=async (req,res,next)=>{}
+const LoginUser=async (req,res,next)=>{
+    const {email,password }=req.body
+    const existingUser=await User.findOne({email})
+    if(!existingUser){
+        throw new BadRequestError("sorry user not found")
+    }
+    const matchPassword= existingUser.isPasswordCorrect(password)
+    if(!matchPassword){
+        throw new BadRequestError("Invalid Credientials !")
+    }
+
+
+
+}
 const LogoutUser=async (req,res,next)=>{}
---
+++ model/index.js
@@ -0,0 +1,4 @@
+const User=require("./user.model")
+
+
+module.exports={User}
